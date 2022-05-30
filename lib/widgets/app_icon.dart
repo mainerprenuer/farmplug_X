a7e23e8a3c76d22ae8aca539faf0213e0cebad50
1410310031 <moustymainer@gmail.com>
2022-05-30 17:05:17 +0100

    Recommended _food_fage

--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/cupertino.dart';
-import 'package:godelicious/utils/dimensions.dart';
 
 class AppIcon extends StatelessWidget {
   final IconData icon;
   final Color backgroundColor;
   final Color iconColor;
   final double size;
+  final double iconSize;
   const AppIcon(
       {Key? key,
       required this.icon,
       this.backgroundColor = const Color(0xFFfcf4e4),
       this.iconColor = const Color(0xFF756d54),
-      this.size = 40})
+      this.size = 40,
+      this.iconSize = 16})
       : super(key: key);
 
   @override
@@ -22,11 +23,7 @@ class AppIcon extends StatelessWidget {
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(size / 2),
           color: backgroundColor),
-      child: Icon(
-        icon,
-        color: iconColor,
-        size: Dimension.iconSize16,
-      ),
+      child: Icon(icon, color: iconColor, size: iconSize),
     );
   }
 }
